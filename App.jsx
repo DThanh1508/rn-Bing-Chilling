47c4925e8e5f4498903cc194670eb181d548b29d
DThanh1508 <athanh2002kt@gmail.com>
2022-12-02 15:23:18 +0700

    Mucsic Application

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () =>{
+  return(
+    <View>
+      <Text>Dumamama may!</Text>
+    </View>
+  );
+};
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# rn-bing-chilling
